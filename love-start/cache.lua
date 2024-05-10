743ac3679207ff8c347a7a70d21b52586700d3b4
sekta <akovlevmihail535@gmail.com>
2024-05-10 23:19:47 +0400

    Cache System

---
+++ love-start/cache.lua
@@ -0,0 +1,35 @@
+local lg = love.graphics
+
+--[[----------------------]]--
+
+local cache = {}
+
+cache.imagePath = ""
+cache.fontPath = ""
+
+function cache.setImagePath(path) cache.imagePath = path end
+function cache.setFontPath(path) cache.fontPath = path end
+
+cache.listImages = {}
+cache.listFont = {}
+
+function cache.image(path)
+    path = cache.imagePath .. path
+    local img = cache.listImages[path]
+    if img then return img end
+
+    cache.listImages[path] = lg.newImage(path)
+end
+
+function cache.font(path, name, size)
+    path = cache.fontPath .. path
+
+    local font = lg.newFont(path, size)
+    cache.listFont[name] = font
+end
+
+function cache.getFont(name)
+    return cache.listFont[name]
+end
+
+return cache
--- love-start/init.lua
+++ love-start/init.lua
@@ -7,6 +7,7 @@ lstart.__index = lstart
 
 lstart.utils = require(PATH .. "utils")
 lstart.lume = require(PATH .. "lume")
+lstart.cache = require(PATH .. "cache")
 lstart.window = require(PATH .. "window")
 
 return setmetatable({}, lstart)
